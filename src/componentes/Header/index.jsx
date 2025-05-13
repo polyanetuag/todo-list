f206f0ed0f75eada50a89b20f16a8be23059aacc
Polyane Tuag <pollyportugues@gmail.com>
2025-05-13 19:03:11 -0300

    feat: add Header component with styles for title and description

---
+++ src/componentes/Header/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export function Header() {
+  return (
+    <div className="header">
+      <h1 className="header-title">Meu To-Do List</h1>
+      <p className="header-description">Lista de produtos</p>
+    </div>
+  );
+}
