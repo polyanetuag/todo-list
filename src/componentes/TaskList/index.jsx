73003a79d0192d865a91eff398b42d1689186dc1
Polyane Tuag <pollyportugues@gmail.com>
2025-05-14 19:12:48 -0300

    feat: refactor TaskList component to include task toggling and add task functionality

--- src/componentes/TaskList/index.jsx
+++ src/componentes/TaskList/index.jsx
@@ -2,26 +2,50 @@ import { useState } from "react";
 import { Task } from "../Task";
 
 import "./styles.css";
-
-const initialTask = [
-  { id: 1, text: "Aprender React", done: false },
-  { id: 2, text: "Aprender logica", done: false },
-  { id: 3, text: "Praticar CSS", done: true },
-  { id: 4, text: "Aprender Componentização", done: false },
-];
+import { AddTask } from "../AddTask";
 
 export function TaskList() {
-  const [tasks, setTasks] = useState(initialTask);
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Aprender React", done: false },
+    { id: 2, text: "Aprender logica", done: false },
+    { id: 3, text: "Praticar CSS", done: true },
+    { id: 4, text: "Aprender Componentização", done: false },
+  ]);
+
+  const toggleTask = (id) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, done: !task.done };
+        }
+        return task;
+      })
+    );
+  };
+
+  const handleAddTask = (taskText) => {
+    setTasks([
+      {
+        id: crypto.randomUUID(),
+        text: taskText,
+        done: false,
+      },
+      ...tasks,
+    ]);
+  };
 
   return (
-    <ul className="tasks-list">
-      {tasks.map((task, index) => {
-        return (
-          <li key={index}>
-            <Task task={task} />
-          </li>
-        );
-      })}
-    </ul>
+    <section>
+      <AddTask onAddTask={handleAddTask} />
+      <ul className="tasks-list">
+        {tasks.map((task, index) => {
+          return (
+            <li key={index}>
+              <Task task={task} quandoClicarNaTask={toggleTask} />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
