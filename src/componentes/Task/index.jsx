9a9832b82a844c3c3b60b6f493418ea6a9fa6858
Polyane Tuag <pollyportugues@gmail.com>
2025-05-14 19:12:42 -0300

    feat: add click handler to Task component for checkbox interaction

--- src/componentes/Task/index.jsx
+++ src/componentes/Task/index.jsx
@@ -1,9 +1,16 @@
 import "./styles.css";
 
-export function Task({ task }) {
+export function Task({ task, quandoClicarNaTask }) {
   return (
     <div className="task">
-      <input type="checkbox" checked={task.done} className="input-checkbox" />
+      <input
+        type="checkbox"
+        checked={task.done}
+        className="input-checkbox"
+        onChange={() => {
+          quandoClicarNaTask(task.id);
+        }}
+      />
       <p>{task.text}</p>
     </div>
   );
